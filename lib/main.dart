328ebe81d2b9a3e27517297bdf52ef903cc8aaef
Waleed Aftab <90130998+WaleedAftab59@users.noreply.github.com>
2021-10-09 11:59:35 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue), home: LoginPage());
+  }
+}
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Assignment No 5'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(1),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Jawan Pakistan',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                     Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(5),
+                    child: const Text(
+                      'Login Form',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 20),
+                    )),
+                Container(
+                  padding: const  EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter Your Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter Your Password',
+                    ),
+                  ),
+                ),
+                // TextButton(
+                //   onPressed: (){
+                //     //forgot password screen
+                //   },
+                //   child: const Text('Forgot Password'),
+                // ),
+                Container(
+                   // height: 50,
+                    padding: const  EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: ElevatedButton(
+                      child: const Text('Signin'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                      },
+                    )),
+                     TextButton(
+                  onPressed: (){
+                    //forgot password screen
+                  },
+                  child: const Text('Click me for Signup'),
+                ),
+                // Container(
+                //     child: Row(
+                //       children: <Widget>[
+                //        const  Text('Does not have account?'),
+                //         ElevatedButton(
+                //           child: const Text(
+                //             'Sign in',
+                //             style: TextStyle(fontSize: 20),
+                //           ),
+                //           onPressed: () {
+                //             //signup screen
+                //           },
+                //         )
+                //       ],
+                //       mainAxisAlignment: MainAxisAlignment.center,
+                //     ))
+              ],
+            )));
+  }
+}
